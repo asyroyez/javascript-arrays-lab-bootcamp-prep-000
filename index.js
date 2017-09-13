1efd588f32162d478c182e8c45b4a9f8be12ff55
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-09-13 11:53:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   var kittens = ["Milo", "Otis", "Garfield"]
-  kittens.push(name);
+  kittens.push()
   return kittens
 }
